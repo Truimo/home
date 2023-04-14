081b5cc8bb123c6ee07bb00ec98fe06f3606b299
Truimo <xiaomo@20mo.cn>
2023-04-14 23:23:52 +0800

    style: adjust formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
-import {ReactNode} from 'react'
-import {Metadata} from 'next'
+import "./globals.css";
+import {ReactNode} from "react";
+import {Metadata} from "next";
 
 export const metadata: Metadata = {
     title: '我的学习笔记',
