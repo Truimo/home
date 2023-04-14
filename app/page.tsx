c91ff03c381b37d5158cb0659f553b8af9e81d5f
Truimo <xiaomo@20mo.cn>
2023-04-14 22:33:18 +0800

    fix: correct year

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <div className="min-h-full w-full flex flex-col items-center content-center justify-center">
                 <div className={`${inter.className} text-3xl mb-3 font-medium`}>Hello, 欢迎来到我的主页</div>
                 <div className={`${notoSans.className} text-xs flex flex-row items-center justify-center flex-wrap gap-1`}>
-                    <p>Copyright © 2022 <Link href="/">Truimo</Link>. All Right Reserved.</p>
+                    <p>Copyright © 2023 <Link href="/">Truimo</Link>. All Right Reserved.</p>
                     <p><a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">湘ICP备2020021033号-1</a></p>
                 </div>
             </div>
