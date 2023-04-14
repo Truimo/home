c217bfe3805979e45d407c233ee67f1505e9223d
Truimo <xiaomo@20mo.cn>
2023-04-14 23:31:21 +0800

    feat: site config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,30 @@
 import "./globals.css";
 import {ReactNode} from "react";
 import {Metadata} from "next";
+import config from "@/site.config.cjs";
 
 export const metadata: Metadata = {
-    title: '我的学习笔记',
-    description: 'Truimo`s homepage.',
+    title: {
+        default: config.title,
+        template: `%s | ${config.siteName}`
+    },
+    description: config.description,
     viewport: {
         width: 'device-width',
         initialScale: 1,
         minimumScale: 1,
         maximumScale: 1,
         userScalable: false,
+    },
+    openGraph: {
+        type: 'website',
+        locale: 'zh-Hans',
+        title: config.title,
+        description: config.description,
+    },
+    robots: {
+        index: true,
+        follow: true,
     }
 }
 
