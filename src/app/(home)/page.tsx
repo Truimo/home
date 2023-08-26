a34a835590b7a75801986dd4c7eb79de565210a1
Truimo <Xiaomo@20mo.cn>
2023-08-26 19:15:11 +0800

    feat: homepage content

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,15 +2,6 @@ import type {FC, PropsWithChildren} from 'react'
 import {interFont} from '@/lib/fonts'
 import {clsxm} from '@/lib/helper'
 
-export default function Home() {
-    return (
-        <Screen>
-            <div className="min-h-full w-full flex flex-col items-center content-center justify-center">
-                <div className={`${interFont.className} text-3xl mb-3 font-medium`}>Hello, 欢迎来到我的主页</div>
-            </div>
-        </Screen>
-    )
-}
 
 const Screen: FC<PropsWithChildren<{
     className?: string
@@ -19,5 +10,19 @@ const Screen: FC<PropsWithChildren<{
         <div className={clsxm('relative flex h-screen flex-col center', className)}>
             {children}
         </div>
+    )
+}
+
+export default function Home() {
+    return (
+        <Screen>
+            <div className="min-h-full w-full flex flex-col items-center content-center justify-center">
+                <div className={`${interFont.className} text-3xl mb-3 font-medium leading-relaxed`}>
+                    <p>Hello, I'm <span className="text-sky-500 font-bold">Truimo</span>.</p>
+                    <p>Nice to meet you!</p>
+                    <p>Construction is underway...</p>
+                </div>
+            </div>
+        </Screen>
     )
 }
