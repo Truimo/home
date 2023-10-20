d357b4d4dca0e04850fe0e7bfa8d9a63ed417111
Truimo <Xiaomo@20mo.cn>
2023-10-21 00:49:53 +0800

    fix: link a underline

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -23,7 +23,7 @@ const links: Link[] = [
 
 export default function Page() {
     return (
-        <div className="">
+        <div>
             <h1 className="text-3xl font-bold leading-relaxed">朋友们</h1>
             <h3 className="text-lg font-semibold leading-relaxed">海内存知己，天涯若比邻</h3>
             <section className="mt-10 grid grid-cols-1 gap-6 md:grid-cols-2 2xl:grid-cols-3">
@@ -39,7 +39,7 @@ const Link: FC<{ data: Link }> = (props) => {
     const {name, description, url, avatar} = props.data
     return (
         <div className="p-3 rounded-lg hover:bg-slate-200/80">
-            <a className="flex items-center" href={url} target="_blank" role="link">
+            <a className="flex items-center hover:no-underline" href={url} target="_blank" role="link">
                 <div className="h-16 w-16 rounded-full border overflow-hidden">
                     <img className="aspect-square" src={avatar} height="64" width="64" alt="avatar" loading="lazy"/>
                 </div>
