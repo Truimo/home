2ff19c011489866af04a11f276ab607ce4f93d5d
Truimo <xiaomo@20mo.cn>
2023-10-20 19:05:52 +0800

    fix: avatar image load fail

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import clsx from 'clsx'
 import {interFont, titilliumWebFont} from '@/lib/fonts'
 
@@ -7,7 +6,7 @@ export default function Home() {
         <div className="profile flex justify-center items-center">
             <div className={`${interFont.className}`}>
                 <div className="inline-block h-[150px] w-[150px] rounded-full border overflow-hidden">
-                    <Image src="https://assets.truimo.com/avatars/min.png" alt="avatar" width={150} height={150}/>
+                    <img src="https://assets.truimo.com/avatars/min.png" alt="avatar" width={150} height={150}/>
                 </div>
                 <h1 className="text-3xl font-bold py-4">TRUIMO</h1>
                 <p className={clsx(titilliumWebFont.className, 'text-lg')}>Wonderful things will happen.</p>
