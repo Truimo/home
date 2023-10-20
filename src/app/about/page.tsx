cd6e5530e9d482379015cc54b788e57bd5ca5829
Truimo <Xiaomo@20mo.cn>
2023-10-21 01:13:11 +0800

    fix: about page metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@
 export default function Page() {
     return (
         <div>
-            <h1 className="text-3xl font-bold leading-relaxed">About</h1>
+            <h1 className="text-3xl font-bold leading-relaxed">关于</h1>
             <div className="mt-4">
                 <h3 className="text-lg font-semibold leading-relaxed">Contact me</h3>
                 <p className="mt-2">Email: <a href="mailto:truimo@qq.com">Send me an email</a></p>
