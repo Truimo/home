9a262ae848b2737ce25d878f8c85621a6e3dab7c
Truimo <Xiaomo@20mo.cn>
2023-12-13 23:07:21 +0800

    feat: update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,11 +9,7 @@ export default function Page() {
             <p className="leading-relaxed">喜欢阅读，常阅读科幻、悬疑类</p>
             <p className="leading-relaxed">喜欢音乐，学废过乐器，以听音乐为主</p>
             <h3 className="text-lg font-semibold leading-loose">Contact</h3>
-            <p className="leading-relaxed">电子邮件: <a href="mailto:truimo@qq.com">truimo@qq.com</a></p>
-            <h2 className="text-2xl font-bold leading-loose">Website</h2>
-            <p className="leading-relaxed">它基于 Next.js 框架，使用 Tailwind CSS 进行设计，使用 TypeScript 进行开发，使用 Vercel 进行部署。</p>
-            <p className="leading-relaxed">这是我的个人网站，很久没有维护了，现在我又把它重拾起来。</p>
-            <p className="leading-relaxed">在这会记录关于我的一些事情，受某些原因的影响，这里依然保持简洁。</p>
+            <p className="leading-relaxed">电子邮件: <a href="mailto:i@truimo.com">i@truimo.com</a></p>
             <h2 className="text-2xl font-bold leading-loose">Domain</h2>
             <p className="leading-relaxed">当然是英文 ID 加 com 作为域名啦，好像没有别的特色了。</p>
             <div className="h-10"></div>
