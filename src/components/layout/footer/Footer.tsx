c15632d77be488ad6ee8966dfef20b44f7bdfe7c
Truimo <Xiaomo@20mo.cn>
2024-02-27 22:29:14 +0800

    feat: footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer>
             <div className="h-16 max-w-6xl mx-auto flex justify-center items-center">
-                <p className="text-center text-sm text-zinc-500">Copyright&nbsp;&copy;&nbsp;2024&nbsp;<a href="/">{owner.name}</a>.&nbsp;<a>Home</a>.&nbsp;
+                <p className="text-center text-sm text-zinc-500">Copyright&nbsp;&copy;&nbsp;2018-2024&nbsp;<a href="/">{owner.name}</a>.&nbsp;<a>Home</a>.&nbsp;
                     {icp && (<a href={icp.link} rel="nofollow noreferrer" target="_blank">{icp.text}</a>)}</p>
             </div>
         </footer>
