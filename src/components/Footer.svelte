3d40c9ad29358cf99cd52c64ada11defe8b5996b
Truimo <xiaomo@20mo.cn>
2025-04-14 02:09:33 +0800

    fix: footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
     <div class="h-16 max-w-6xl mx-auto flex justify-center items-center">
-        <p class="text-center text-sm text-zinc-500">
+        <p class="text-center text-sm">
             <span>Copyright&nbsp;&copy;&nbsp;2018-2024&nbsp;<a href="/">Truimo's Home</a>.&nbsp;</span>
             <a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">湘ICP备2020021033号-1</a>
         </p>
