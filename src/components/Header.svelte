7e4d11e513e0567b71a19d2f96d1c6ff320eab44
Truimo <xiaomo@20mo.cn>
2025-04-14 01:56:05 +0800

    fix: header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,11 @@
 <header>
     <nav class="h-16 max-w-6xl mx-auto flex justify-between items-center">
         <div class="h-full mx-4 flex items-center">
-            <a class="text-2xl font-bold" href="/" title="浅小沫">TRUIMO</a>
+            <a class="text-2xl font-bold" href="/" title="浅小沫">TRUIMO<span class="text-base">.com</span></a>
         </div>
         <ul class="h-full mx-4 text-lg flex items-center">
             <li class="ms-4 h-full flex items-center">
-                <a href="/about" title="About">About</a>
+                <a href="/about" title="关于">About</a>
             </li>
         </ul>
     </nav>
