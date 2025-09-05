ae2b384f7d880a796e1540d5f300d961cfc4f8b1
Truimo <Xiaomo@20mo.cn>
2025-09-05 18:18:38 +0800

    feat: avatar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class="profile flex justify-center items-center">
     <div>
         <div class="avatar inline-block rounded-full border overflow-hidden">
-            <img src="https://assets.truimo.com/avatars/min.png" alt="avatar" width={150} height={150}/>
+            <img src="./min.png" alt="avatar" width={150} height={150}/>
         </div>
         <h1 class="text-3xl font-bold py-4">TRUIMO</h1>
         <p class="text-lg">Wonderful things will happen.</p>
