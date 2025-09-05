3b081bfe94f81483ea5213f794472e5cfed02ffb
Truimo <Xiaomo@20mo.cn>
2025-09-05 18:24:02 +0800

    feat: footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer>
     <div class="h-16 max-w-6xl mx-auto flex justify-center items-center">
         <p class="text-center text-sm">
-            <span>Copyright&nbsp;&copy;&nbsp;2018-2025&nbsp;<a href="/">Truimo's Home</a>.&nbsp;</span>
+            <span>Copyright&nbsp;&copy;&nbsp;2018-2025&nbsp;<a href="https://www.truimo.com/">Truimo's Home</a>.&nbsp;</span>
             <a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">湘ICP备2020021033号-1</a>
         </p>
     </div>
