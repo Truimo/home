c2a02ed25f2416eae162131285971467560a31ee
Xiaomo <Xiaomo@20mo.cn>
2025-09-23 22:46:21 +0800

    feat: new content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+</script>
+
 <svelte:head>
     <title>浅小沫 | Truimo - 我的学习笔记</title>
     <meta name="description" content="浅小沫的个人网站，Truimo`s Home Page." />
@@ -6,13 +9,55 @@
 </svelte:head>
 
 <div class="profile flex justify-center items-center">
-    <div>
+    <section class="sr-only">
         <div class="avatar inline-block rounded-full border overflow-hidden">
             <img src="./min.png" alt="avatar" width={150} height={150}/>
         </div>
         <h1 class="text-3xl font-bold py-4">TRUIMO</h1>
         <p class="text-lg">Wonderful things will happen.</p>
-    </div>
+    </section>
+    <section class="space-y-9 text-base">
+        <div class="space-y-3">
+            <p>人活着，是需要动力的。</p>
+            <p>没有什么支撑的话，活着会很痛苦。</p>
+        </div>
+        <div class="space-y-3">
+            <p>我还年轻，很少有机会考虑这个问题 ......</p>
+            <p>后来不得不考虑这个问题了，</p>
+            <p>于是我经过思考得到了答案。</p>
+        </div>
+        <div>
+            <p>“ — — 人活着是需要动力，但不需要那么多。”</p>
+        </div>
+        <div>
+            <p>大部分人活着，依靠的是惯性。</p>
+        </div>
+        <div class="space-y-3">
+            <p>比如我 — —</p>
+            <p>只要还能做卷子，就能活下去。</p>
+        </div>
+        <div>
+            <p>“刷题百遍，其义自见。”</p>
+        </div>
+        <div class="space-y-3">
+            <p>曾几何时，我将这句话奉为圭臬，</p>
+            <p>并一直以身作则地践行这句话。</p>
+        </div>
+        <div class="space-y-3">
+            <p>哪怕世界马上就要毁灭，</p>
+            <p>只要面前摆着一张卷子，我就能拿起笔把它做完。</p>
+        </div>
+        <div class="space-y-3">
+            <p>选择题填空题判断题问答题，</p>
+            <p>小题大题作文题 ......</p>
+        </div>
+        <div>
+            <p>它们几乎成了我活下去的全部意义。</p>
+        </div>
+        <div>
+            <p>《梦末》</p>
+        </div>
+    </section>
 </div>
 
 <style>
